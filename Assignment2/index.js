bafaff42c540f20d2ebb14c8360c51794444bd8d
Sanjida Akter <137642044+Sanjida51@users.noreply.github.com>
2024-07-02 20:49:10 +0600

    Add files via upload

---
+++ Assignment2/index.js
@@ -0,0 +1,48 @@
+/*let book = {
+  title: "To Kill a Mockingbird",
+  author: "Harper Lee",
+  year: 1960
+};
+
+/*console.log(book.title); // Using dot notation
+console.log(book['author']); // Using bracket notation
+console.log(book.year);*/
+
+/*book.title = "1984";
+book.author = "George Orwell";
+book.year = 1949;
+
+console.log(book);*/
+/*function displayObjectProperties(obj) {
+    let keys = Object.keys(obj);
+    let values = Object.values(obj);
+    let result = '';
+
+    for (let i = 0; i < keys.length; i++) {
+        result += `${keys[i]}: ${values[i]}, `;
+    }
+    
+    return result.slice(0, -2); // Removing the last comma and space
+}
+
+let book = {
+    title: "1984",
+    author: "George Orwell",
+    year: 1949
+};
+
+console.log(displayObjectProperties(book));
+*/
+/*function Book(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+}
+
+let book1 = new Book("1984", "George Orwell", 1949);
+let book2 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
+
+console.log(book1);
+console.log(book2);
+*/
+
